ad9a94336ef5cb717a856c24897b08696e717bde
Simon de Haan <simon@praekeltfoundation.org>
2015-10-05 16:38:22 +0200

    add db:migrate script to postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,4 @@
 cd /var/praekelt/goddard-hub-server
 npm install
-DB_URL=postgres://postgres:@localhost/inmarsat node index.js --migrations --seed
+DB_URL=postgres://postgres:@localhost/inmarsat node_modules/.bin/sequelize db:migrate
 cd -
